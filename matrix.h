a3bd5030fa2d3b058a74818576aaae7874037a69
Teiroba <114053995+Teiroba@users.noreply.github.com>
2022-09-21 15:47:28 +0200

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -19,6 +19,7 @@ scalar *matrix_get(matrix m, unsigned i, unsigned j);
 matrix matrix_add(matrix m, matrix n);
 matrix matrix_mul(matrix m, matrix n);
 matrix matrix_power(matrix m, unsigned p);
+matrix scalar_multiplication(matrix m, scalar k);
 void matrix_print(FILE *f, matrix m);
 
 matrix add_matrix(matrix m, matrix n);
